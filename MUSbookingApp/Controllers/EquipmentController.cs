832d4390506210703f9c011c766fbf42a72e92cd
Daria <ferra@list.ru>
2024-05-24 17:43:52 +0300

    fix: add controllers

---
+++ MUSbookingApp/Controllers/EquipmentController.cs
@@ -0,0 +1,21 @@
+﻿using BAL.Dto.Equipment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MUSbookingApp.Controllers
+{
+    [Route("equipment")]
+    [ApiController]
+    public class EquipmentController : Controller
+    { 
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        //Создание оборудования
+        public async Task<ActionResult<EquipmentDto>> Create(CancellationToken cancellationToken)
+        {
+            return Ok();
+        }
+
+
+    }
+}
--- MUSbookingApp/Controllers/OrderController.cs
+++ MUSbookingApp/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BAL.Dto.Order;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MUSbookingApp.Controllers
 {        
@@ -6,20 +7,43 @@ namespace MUSbookingApp.Controllers
     [ApiController]
     public class OrderController : Controller
     {
-        //Создание оборудования
-        //Создание заказа
-        //Обновление заказа
-        //Удаление заказа
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         //Вывод списка всех заказов, отсортированных по дате создания с пагинацией
+        public async Task<ActionResult<OrderDto>> GetOrderPagination(CancellationToken cancellationToken)
+        {
+            return Ok();
+        }
 
         [HttpPost]
         [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
+        [ProducesResponseType(400)]        
+        //Создание заказа
         public async Task<ActionResult<OrderDto>> Create([FromBody] OrderCreateDto request, CancellationToken cancellationToken)
         {
-            var result = await _applicationService.CreateApplicationAsync(request, cancellationToken);
 
-            return result.ToActionResult();
+            return Ok();
         }
+
+        [HttpPut]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        //Обновление заказа
+        public async Task<ActionResult<OrderDto>> Update(CancellationToken cancellationToken)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        //Удаление заказа
+        public async Task<ActionResult> Delete (CancellationToken cancellationToken)
+        {
+            return Ok();
+        }
+
+
     }
 }
