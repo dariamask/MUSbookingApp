64e2dfaccc203504e6b1df9379efdd4baf8fea5d
Daria <ferra@list.ru>
2024-05-25 14:58:43 +0300

    fix: add exception handling middleware

---
+++ MUSbookingApp/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MUSbookingApp.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(
+            RequestDelegate requestDelegate,
+            ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _requestDelegate(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occured: {ErrorMessage}", ex.Message);
+
+                var problemDetails = new ProblemDetails
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Title = "Service is unavailable",
+                    Type = ""
+                };
+
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                await httpContext.Response.WriteAsJsonAsync(problemDetails);
+            }
+        }
+    }
+}
